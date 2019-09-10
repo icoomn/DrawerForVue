b302e15f3d716bd8f78d1d041ccd1f68eaa7aff5
yunfeng <281542025@qq.com>
2019-09-11 00:13:45 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button class="btn open" @click="display = true">点我，打开抽屉</button>
+    <drawer title="Hello, I'm drawer" :display.sync="display" :width="drawerWidth">
+        <p>1. Hello, world!</p>
+        <p>2. Do you like it?</p>
+        <button class="btn try" @click="changeWidth">点我试试?</button>
+    </drawer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import drawer from './components/drawer'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    drawer
+  },
+  data () {
+    return {
+        display: false,
+        drawerWidth: '500px'
+    }       
+  },
+  methods: {
+    changeWidth () {
+      this.drawerWidth = (this.drawerWidth === '500px') ? '800px' : '500px'
+    }
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px;
+}
+.btn { 
+  border: none;
+  border-radius: 3px;
+  outline: none;
+  cursor: pointer;
+  letter-spacing: 3px;
+  &:hover {
+    opacity: .8;
+  }
+  &.open {
+    width: 200px;
+    height: 80px;
+    font-size: 18px;
+    background-color: orange;
+    color: #fff;
+  }
+  &.try {
+    width: 100px;
+    height: 40px;
+    background-color: green;
+    color: #fff;
+    margin-top: 50px;
+  }
 }
 </style>
